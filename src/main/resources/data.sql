238028ae18f9c28b58aa5f647f2598a86eb4a3b0
DESKTOP-V1FUODV\ajay_admin <>
2019-02-24 13:44:27 -0500

    corrected the sql error to generate entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+CREATE TABLE todoentity (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+	status INTEGER,
+    PRIMARY KEY (id)
+);
 insert into todoentity (id,name,status) values (1001,'my first todo item-1',0);
 insert into todoentity (id,name,status) values (1002,'my 2nd todo item-2',1);
 insert into todoentity (id,name,status) values (1003,'my 3rd todo item-3',1);
